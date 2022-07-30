b32a0b654914666942959494d11c78b9e60fb74f
pantera2580 <emmnlcruz@gmail.com>
2022-07-30 10:09:05 -0300

    basic experience

---
+++ src/app/model/experience/experience-response.spec.ts
@@ -0,0 +1,7 @@
+import { ExperienceResponse } from './experience-response';
+
+describe('ExperienceResponse', () => {
+  it('should create an instance', () => {
+    expect(new ExperienceResponse()).toBeTruthy();
+  });
+});
---
+++ src/app/model/experience/experience-response.ts
@@ -0,0 +1,9 @@
+export class ExperienceResponse {
+  id: string = ''
+  company: string = ''
+  job: string = ''
+  initialDate!: Date
+  finishDate!: Date
+  description: string = ''
+  personId: string = ''
+}
